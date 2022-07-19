d51f07037ec9ed1a6596e52228bc7c9cb9ff34db
Ross McFarland <rwmcfa1@gmail.com>
2022-07-19 13:05:09 -0700

    wip go mpegts

--- app.go
+++ app.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/binary"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/asticode/go-astits"
 )
 
 func check(e error) {
@@ -94,9 +99,102 @@ func (t *Transcoder)Transcode(filename string) {
 		t.atomize(buf, frameCh)
 	}()
 
-	for frame := range frameCh {
-		fmt.Printf("received frame=*** %d\n", len(frame))
+	// Create a cancellable context in case you want to stop writing packets/data any time you want
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Handle SIGTERM signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM)
+	go func() {
+		<-ch
+		cancel()
+	}()
+
+	// Create your file or initialize any kind of io.Writer
+	// Buffering using bufio.Writer is recommended for performance
+	f, _ := os.Create("tmp/stream.ts")
+	defer f.Close()
+
+	// Create the muxer
+	mx := astits.NewMuxer(ctx, f)
+
+	// Add an elementary stream
+	err = mx.AddElementaryStream(astits.PMTElementaryStream{
+		ElementaryPID: 257,
+		StreamType:    astits.StreamTypeAACAudio,
+	})
+	check(err)
+
+	mx.SetPCRPID(257)
+
+	// Write tables
+	// Using that function is not mandatory, WriteData will retransmit tables from time to time
+	mx.WriteTables()
+
+	af := &astits.PacketAdaptationField{
+		RandomAccessIndicator: true,
+		HasPCR: true,
+		PCR: &astits.ClockReference{Base: int64(0)},
 	}
+
+	faac, err := os.Create("tmp/stream-go.aac")
+    check(err)
+
+	for frame := range frameCh {
+		n := len(frame)
+		fmt.Printf("received frame=*** %d\n", n)
+		// include the header size
+		n += 7
+
+		header := []byte{
+			// 1111 1111 - 0
+			0xff,
+			// 1111 0001 - 1
+			0xf1,
+			// 0101 0000 - 2
+			0x50,
+			// 1000 00xx - 3
+			0x80,
+			// xxxx xxxx - 4
+			0x00,
+			// xxx1 1111 - 5
+			0x1f,
+			// 1111 1100 - 6
+			0xfc,
+		}
+		fmt.Printf("before header=%x %b\n", header, header)
+		header[3] |= byte(n >> 11)
+		header[4] |= byte(n >> 3)
+		header[5] |= byte(n << 5)
+		fmt.Printf(" after header=%x %b\n", header, header)
+
+		n, err := faac.Write(append(header, frame...))
+		check(err)
+
+		fmt.Printf("%x + %x = %x\n", header, frame[:4], append(header, frame[:4]...))
+
+		// Write data
+		n, err = mx.WriteData(&astits.MuxerData{
+			PID: 257,
+			AdaptationField: af,
+			PES: &astits.PESData{
+				Header: &astits.PESHeader{
+					OptionalHeader: &astits.PESOptionalHeader{
+						MarkerBits:      2,
+						PTSDTSIndicator: astits.PTSDTSIndicatorOnlyPTS,
+						PTS:             &astits.ClockReference{Base: int64(0)},
+					},
+					PacketLength: uint16(n + 8),
+					StreamID:     192, // = audio
+				},
+				Data: append(header, frame...),
+			},
+		})
+		check(err)
+		fmt.Printf("  wrote %d\n", n)
+	}
+
+	cancel()
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ross/youtube-proxy
 
 go 1.18
+
+require (
+	github.com/asticode/go-astikit v0.20.0 // indirect
+	github.com/asticode/go-astits v1.10.0 // indirect
+)
